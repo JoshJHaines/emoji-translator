bad06366e65c6fb68481548c2ab4af04649defd4
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-25 14:24:45 -0400

    was calling the wrong class for radio buttons. changed and if statement to decifer which radio selected works

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 const submitButton = document.getElementById("submit-button")
 const translatorInput = document.getElementById("translator-input")
 const results = document.getElementById("results")
+const radios = document.querySelectorAll(".radio-button")
 
 
 /*************************
@@ -17,7 +18,15 @@ submitButton.addEventListener('click', submitClick)
  ************/
 function submitClick(){
     let inputString = translatorInput.value
+
+    for (radio of radios){
+        if (radio.checked === true){
+            console.log("Radio Checked:", radio.value)
+        }
+    }
     console.log("The Submit Button WAS clickeded!")
     console.log("trans input:", inputString)
     results.innerText = encode(inputString)
+
+
 }
