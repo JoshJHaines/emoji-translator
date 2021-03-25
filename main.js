94b1fe872db89a701d0bf95814ce0bff6494b928
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-25 13:34:41 -0400

    created query for button and click event listener. Prints message in console

---
+++ main.js
@@ -0,0 +1,8 @@
+const submitButton = document.getElementById("submit-button")
+
+submitButton.addEventListener('click', submitClick)
+
+
+function submitClick(){
+    console.log("The Submit Button WAS clickeded!")
+}
