84567cef176ac53f0a4ae5890524f5cea646d313
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-25 13:45:07 -0400

    Created query selector for input box and saved it as a variable in the function

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+
+/*******************
+ * QUERY SELECTORS *
+ *******************/
 const submitButton = document.getElementById("submit-button")
+const translatorInput = document.getElementById("translator-input")
 
+
+/*************************
+ * CLICK EVENT LISTENERS *
+ *************************/
 submitButton.addEventListener('click', submitClick)
-
-
+ 
+/************
+ * FUNCTION *
+ ************/
 function submitClick(){
+    let inputString = translatorInput.value
     console.log("The Submit Button WAS clickeded!")
+    console.log("trans input:", inputString)
 }
